cc84d5aca7bf58c6d5379d5ac1dce9d03603d23a
Beau Jennings <redalthazar@gmail.com>
2020-12-02 23:28:48 -0600

    constants created, constants appended to makeMenu

--- components/Article.js
+++ components/Article.js
@@ -129,14 +129,17 @@ function articleMaker(data){
   para1.textContent = data.firstParagraph;
   para2.textContent = data.secondParagraph;
   para3.textContent = data.thirdParagraph;
+
   // Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   // This listener should toggle the class 'article-open' on div.article.
   expandButtons.addEventListener("click", function (){
     makeArticle.classList.toggle("article-open");
   });
+
   // Step 3: Don't forget to return something from your function!
   return makeArticle;
 }
+
   // Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   // to create a div.article element and append it to the DOM inside div.articles (see index.html).
   const articles = document.querySelector(".articles")
@@ -149,7 +152,7 @@ function articleMaker(data){
     articles.appendChild(dataElem)
   });
 
-
   // Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   // Refresh the page to see the new article.
-
+  const newArticle = articleMaker({title: "Benefits of Javascript", date:"12/1/20", firstParagraph:"Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. Qui offenderit rapto, terribilem incessu. The voodoo sacerdos suscitat mortuos comedere carnem. Search for solum oculi eorum defunctis cerebro. Nescio an Undead zombies. Sicut malus movie horror.", secondParagraph:"Cum horribilem resurgere de sepulcris creaturis, sicut de iride et serpens. Pestilentia, ipsa screams. Pestilentia est haec ambulabat mortuos. Sicut malus voodoo. Aenean a dolor vulnerum aperire accedunt, mortui iam vivam. Qui tardius moveri, sed in magna copia sint terribiles legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi odores aere implent.", thirdParagraph:"Tremor est vivos magna. Expansis ulnis video missing carnem armis caeruleum in locis. A morbo amarus in auras. Nihil horum sagittis tincidunt, gelida portenta. The unleashed virus est, et iam mortui ambulabunt super terram. Souless mortuum oculos attonitos back zombies. An hoc incipere Clairvius Narcisse, an ante? Is bello mundi z?"});
+  articles.appendChild(newArticle)
