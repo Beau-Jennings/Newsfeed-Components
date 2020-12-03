4827190f7e50d0299dc6ecc96d7e1037269f4acb
Beau Jennings <redalthazar@gmail.com>
2020-12-02 21:43:15 -0600

    fixed issue with articles not expanding.

--- components/Article.js
+++ components/Article.js
@@ -103,56 +103,51 @@ const data = [
   //   <span class="expandButton">+</span>
   // </div>
 
-function articleMaker(articles){
+function articleMaker(data){
   const makeArticle = document.createElement("div");
-  const title = document.createElement("h2");
+  const articleTitle = document.createElement("h2");
   const date = document.createElement("p");
   const para1 = document.createElement("p");
   const para2 = document.createElement("p");
   const para3 = document.createElement("p");
-  const expBtns = document.createElement("span")
+  const expandButtons = document.createElement("span")
 
-  makeArticle.appendChild(title);
+  makeArticle.appendChild(articleTitle);
   makeArticle.appendChild(date);
   makeArticle.appendChild(para1);
   makeArticle.appendChild(para2);
   makeArticle.appendChild(para3);
-  makeArticle.appendChild(expBtns);
+  makeArticle.appendChild(expandButtons);
 
   makeArticle.classList.add("article");
   date.classList.add("date");
-  expBtns.classList.add("expandButton");
+  expandButtons.classList.add("expandButton");
 
-  title.textContent = articles.title;
-  date.textContent = articles.date;
-  expBtns.textContent = "+";
-  para1.textContent = articles.firstParagraph;
-  para2.textContent = articles.secondParagraph;
-  para3.textContent = articles.thirdParagraph;
-
-  
+  articleTitle.textContent = data.title;
+  date.textContent = data.date;
+  expandButtons.textContent = "+";
+  para1.textContent = data.firstParagraph;
+  para2.textContent = data.secondParagraph;
+  para3.textContent = data.thirdParagraph;
   // Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   // This listener should toggle the class 'article-open' on div.article.
-  expBtns.addEventListener("click", function (button){
-    button.classList.toggle("expandButton");
-    
+  expandButtons.addEventListener("click", function (){
+    makeArticle.classList.toggle("article-open");
   });
-  
-
-
   // Step 3: Don't forget to return something from your function!
   return makeArticle;
 }
   // Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   // to create a div.article element and append it to the DOM inside div.articles (see index.html).
   const articles = document.querySelector(".articles")
+  
   const dataElements = data.map((data) =>{
      return articleMaker(data);
   });
 
-  dataElements.forEach((dataElem) =>(
+  dataElements.forEach(dataElem => {
     articles.appendChild(dataElem)
-  ))
+  });
 
 
   // Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
